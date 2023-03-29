04cd530e6f57fa112e5a7c6881bc08595b452c76
daevidvo <itsdavidvo@gmail.com>
2023-03-29 16:39:21 -0700

    finished the overall structure ONCE AGAIN and finished writing comments on functionality within script.js

--- script.js
+++ script.js
@@ -55,73 +55,151 @@ function generatePassword() {
   }
   passLengthFunc() // starts the series
   
-  function criteriaSelector() {
-    Number(passLength);
-    if (lc && uc && sc && nc) {
-      for (var x=0;x<passLength;x=x+1) {
-        let y = arrayChooser(4);
-        if (y === 3) {
-          let z = Math.floor(Math.random() * numArray.length);
-          z = numArray[z];
-          finalPassword.unshift(z);
-        } else if (y === 2) {
-          let z = Math.floor(Math.random() * lcArray.length);
-          z = lcArray[z];
-          finalPassword.unshift(z);
-        } else if (y === 1) {
-          let z = Math.floor(Math.random() * ucArray.length);
-          z = ucArray[z];
-          finalPassword.unshift(z);
-        } else if (y === 0) {
-          let z = Math.floor(Math.random() * scArray.length);
-          z = scArray[z];
-          finalPassword.unshift(z);
+  function criteriaSelector() { // this was tedious to make
+    Number(passLength); //takes the input that the user gave us (which will now always be a number if it made it this far) and converts it into an integer
+    if (lc && uc && sc && nc) { //when user answers yes to all prompts this will execute
+      for (var x=0;x<passLength;x=x+1) { //for loop will iterate depending on the number the user has given us
+        let arrayChosen = arrayChooser(4);//arrayChooser() function will generate a random number between 0 and whatever the input (in this case 4) is. This is here to TRULY randomize what characters are going where instead of having it be lowercase letter, uppercase letter, special character, and number, in that pattern until the for loop ends
+        if (arrayChosen === 3) { //if arrayChooser() selects 3, it will trigger the numArrayFunc()
+          numArrayFunc();
+        } else if (arrayChosen === 2) { //if arrayChooser() selects 2, it will trigger lcArrayFunc()
+          lcArrayFunc();
+        } else if (arrayChosen === 1) { //if arrayChooser() selects 1, it will trigger ucArrayFunc()
+          ucArrayFunc();
+        } else if (arrayChosen === 0) { //if arrayChooser() selects 0, it will trigger scArrayFunc()
+          scArrayFunc();
         }
       }
-    } else if (lc && uc && sc) {
+    } else if (lc && uc && sc && !(nc)) { //when the user answers yes to the first three prompts but not the last one
       for (var x=0;x<passLength;x=x+1) {
-        let y = arrayChooser(3);
-        if (y === 2) {
-          let z = Math.floor(Math.random() * lcArray.length);
-          z = lcArray[z];
-          finalPassword.unshift(z);
-        } else if (y === 1) {
-          let z = Math.floor(Math.random() * ucArray.length);
-          z = ucArray[z];
-          finalPassword.unshift(z);
-        } else if (y === 0) {
-          let z = Math.floor(Math.random() * scArray.length);
-          z = scArray[z];
-          finalPassword.unshift(z);
+        let arrayChosen = arrayChooser(3);
+        if (arrayChosen === 2) {
+          lcArrayFunc();
+        } else if (arrayChosen === 1) {
+          ucArrayFunc();
+        } else if (arrayChosen === 0) {
+          scArrayFunc();
         }
       }
-    } else if (lc && uc) {
+    } else if (lc && sc && nc && !(uc)) { //when the user answers yes to the first, third, and last prompt but not the second one
       for (var x=0;x<passLength;x=x+1) {
-        let y = arrayChooser(2);
-        if (y === 1) {
-          let z = Math.floor(Math.random() * lcArray.length);
-          z = lcArray[z];
-          finalPassword.unshift(z);
-        } else if (y === 0) {
-          let z = Math.floor(Math.random() * ucArray.length);
-          z = ucArray[z];
-          finalPassword.unshift(z);
+        let arrayChosen = arrayChooser(3);
+        if (arrayChosen === 2) {
+          lcArrayFunc();
+        } else if (arrayChosen === 1) {
+          scArrayFunc();
+        } else if (arrayChosen === 0) {
+          numArrayFunc();
+        }
       }
+    } else if (uc && sc && nc && !(lc)) { //when the user only answers yes to the last three prompts  
+      for (var x=0;x<passLength;x=x+1) {
+        let arrayChosen = arrayChooser(3);
+        if (arrayChosen === 2) {
+          ucArrayFunc();
+        } else if (arrayChosen === 1) {
+          scArrayFunc();
+        } else if (arrayChosen === 0) {
+          numArrayFunc();
+        }
+      }
+    } else if (lc && uc && !(sc && nc)) { //when the user only answers yes to first two prompts  
+      for (var x=0;x<passLength;x=x+1) {
+        let arrayChosen = arrayChooser(2);
+        if (arrayChosen === 1) {
+          lcArrayFunc();
+        } else if (arrayChosen === 0) {
+          ucArrayFunc();
+        }
+      }
+    } else if (lc && sc && !(uc && nc)) { //when the user only answers yes to the first and third prompt 
+      for (var x=0;x<passLength;x=x+1) {
+        let arrayChosen = arrayChooser(2);
+        if (arrayChosen === 1) {
+          lcArrayFunc();
+        } else if (arrayChosen === 0) {
+          scArrayFunc();
+        }
+      }
+    } else if (lc && nc && !(uc && sc)) { //when the user only answers yes to the first and last prompt 
+      for (var x=0;x<passLength;x=x+1) {
+        let arrayChosen = arrayChooser(2);
+        if (arrayChosen === 1) {
+          lcArrayFunc();
+        } else if (arrayChosen === 0) {
+          numArrayFunc();
+        }
+      }
+    } else if (uc && sc && !(lc && nc)) { //when the user only answers yes to the second and third prompt
+      for (var x=0;x<passLength;x=x+1) {
+        let arrayChosen = arrayChooser(2);
+        if (arrayChosen === 1) {
+          ucArrayFunc();
+        } else if (arrayChosen === 0) {
+          scArrayFunc();
+        }
+      }
+    } else if (uc && nc && !(lc && sc)) { //when the user only answers yes to the second and last prompt
+      for (var x=0;x<passLength;x=x+1) {
+        let arrayChosen = arrayChooser(2);
+        if (arrayChosen === 1) {
+          ucArrayFunc();
+        } else if (arrayChosen === 0) {
+          numArrayFunc();
+        }
+      }
+    } else if (sc && nc && !(lc && uc)) { //when the user only answers yes to the last two prompts 
+      for (var x=0;x<passLength;x=x+1) {
+        let arrayChosen = arrayChooser(2);
+        if (arrayChosen === 1) {
+          scArrayFunc();
+        } else if (arrayChosen === 0) {
+          numArrayFunc();
+        }
+      }
+    } else if (lc && !(uc && sc && nc)){ //when the user answers yes to the first prompt only
+      for (var x=0;x<passLength;x=x+1) {
+        lcArrayFunc();
+      }
+    } else if (uc && !(lc && sc && nc)){ //when the user answers yes to the second prompt only
+      for (var x=0;x<passLength;x=x+1) {
+        ucArrayFunc();
+      }
+    } else if (sc && !(lc && uc && nc)){ //when the user answers yes to the third prompt only
+      for (var x=0;x<passLength;x=x+1) {
+        scArrayFunc();
+      }
+    } else if (nc && !(lc && uc && sc)){ //when the user answers yes to the fourth prompt only
+      for (var x=0;x<passLength;x=x+1) {
+        numArrayFunc();
       }
     } else {
-      for (var x=0;x<passLength;x=x+1) {
-        let z = Math.floor(Math.random() * lcArray.length);
-        z = lcArray[z];
-        finalPassword.unshift(z);
-      }
+      alert("Please choose criterias for your password"); //when the user inputs a number into the password generator but doesn't select any of the criterias
     }
   }
   
-  function arrayChooser(max) {
+  function arrayChooser(max) { //function that generates a random number and that number is used to choose whether a lowercase letter (lcl or lc), uppercase letter (ucl or uc), special character (sc), or number (num or nc) is going to be added to the password depending on the criterias the user has chosen
     return Math.floor(Math.random()*max);
   }
-  
-  return finalPassword.join("");
+
+  function numArrayFunc() { //function for choosing a random index in the numbers array and adds it to the password that is going to be shown
+    finalPassword.unshift(numArray[(Math.floor(Math.random() * numArray.length))]); //makes random number within array length then makes that number the index position within the array, then takes that element value in the array at that position and adds it to finalPassword array
+  }
+
+  function lcArrayFunc() {
+    finalPassword.unshift(lcArray[(Math.floor(Math.random() * lcArray.length))]);
+  }
+
+  function ucArrayFunc() {
+    finalPassword.unshift(ucArray[(Math.floor(Math.random() * ucArray.length))]);
+  }
+
+  function scArrayFunc() {
+    finalPassword.unshift(scArray[(Math.floor(Math.random() * scArray.length))]);
+  }
+
+  //command below is the output of the generatePassword() function
+  return finalPassword.join(""); //.join makes turns the finalPassword array into a string rather than having the array values be separated by a comma when shown to user
 }
 
 // Write password to the #password input
